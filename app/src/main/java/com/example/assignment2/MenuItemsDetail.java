575bb388ddda42ae3cef79f99a15d25a10a8275a
Oliver <oliverjin951@gmail.com>
2019-10-22 21:28:53 +1100

    uoo

--- app/src/main/java/com/example/assignment2/MenuItemsDetail.java
+++ app/src/main/java/com/example/assignment2/MenuItemsDetail.java
@@ -15,53 +15,45 @@ public class MenuItemsDetail extends AppCompatActivity {
     private TextView foodIngredients;
     private TextView addToOrder;
     private TextView addedToOrder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_item_detail);
 
-
-//        Spinner spinner = findViewById(R.id.spinner2);
-//        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.options_array, android.R.layout.simple_spinner_item);
-//        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner.setAdapter(adapter1);
-//        spinner.setOnItemSelectedListener(this);
-
-
         final NumberPicker numberPicker = findViewById(R.id.numberPicker);
         numberPicker.setMinValue(0);
-        numberPicker.setMaxValue(9);
-
-        int value1 = numberPicker.getValue();
-        TextView addToOrder = (TextView)findViewById(R.id.addToOrder);
-        addToOrder.setOnClickListener(new View.OnClickListener(){
+        numberPicker.setMaxValue(15);
+        TextView addToOrder = (TextView) findViewById(R.id.addToOrder);
+        addToOrder.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-            addedToOrder.setText("Added " + numberPicker.getValue() + " to your Order!");
+                addedToOrder.setText("Added " + numberPicker.getValue() + " to your Order!");
             }
         });
-
-
-
 
         Intent intent = getIntent();
         int foodID = intent.getIntExtra("FoodID", 0);
         MenuItems menuItems = Database.getMenuItemByID(foodID);
+
         foodNameText = findViewById(R.id.foodName1);
         foodDescText = findViewById(R.id.foodDescDetail);
         priceText = findViewById(R.id.foodPrice1);
-        foodIngredients= findViewById(R.id.foodIngred1);
+        foodIngredients = findViewById(R.id.foodIngred1);
         addedToOrder = findViewById(R.id.addedToOrder);
-        addToOrder = findViewById(R.id.addToOrder);
+
 
         foodNameText.setText(menuItems.getFoodName());
         foodDescText.setText(menuItems.getFoodDetailedDesc());
         priceText.setText(menuItems.getPrice());
         foodIngredients.setText(menuItems.getFoodDesc());
 
-
-
+        //        Spinner spinner = findViewById(R.id.spinner2);
+//        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.options_array, android.R.layout.simple_spinner_item);
+//        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapter1);
+//        spinner.setOnItemSelectedListener(this);
     }
 }
